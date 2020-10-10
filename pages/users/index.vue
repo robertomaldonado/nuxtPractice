ac3a5cfdc42255e54111656b960e8f2316b416ed
Rob <rbt.maldonado@gmail.com>
2020-10-09 22:01:21 -0400

    Nesting routes

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,24 +1,7 @@
 <template>
   <div>
     <h3>
-      USER NAMES
+      Enter Id and click button
     </h3>
-    <input type="text" v-model="userId">
-    <button @click="onLoadUser"> Load User </button>
   </div>
 </template>
-
-<script>
-export default {
-  data(){
-    return {
-      userId: ""
-    }
-  },
-  methods: {
-    onLoadUser(){
-      this.$router.push("/users/" + this.userId)
-    }
-  }
-}
-</script>
