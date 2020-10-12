8d589d04bcc5fdf463c71986f8cdbf9e9fb21d5f
Rob <rbt.maldonado@gmail.com>
2020-10-12 08:09:29 -0400

    Added custom error site

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        Ups! 
+      </h1>
+      <h2>
+        This is an Error... Please try again!
+      </h2>
+    </header>
+  </div>
+</template>
+
+<style>
+</style>
